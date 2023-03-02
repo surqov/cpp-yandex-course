6fc6804fc93f22694d91ba2f1f860a249383430e
Gleb Surkov <writeln7@gmail.com>
2023-03-02 19:21:32 +0100

    unique_ptr

--- Module_3/17_binary_tree_with_unique_ptr/main.cpp
+++ Module_3/17_binary_tree_with_unique_ptr/main.cpp
@@ -11,7 +11,7 @@ template <typename T>
 using TreeNodePtr = std::unique_ptr<TreeNode<T>>;
 
 template <typename T>
-struct TreeNode {
+struct TreeNode { 
     // Используйте TreeNodePtr<T> вместо сырых указателей
     // Примите умные указатели по rvalue-ссылке
     TreeNode(T val, TreeNodePtr<T>&& left, TreeNodePtr<T>&& right)
@@ -26,53 +26,36 @@ struct TreeNode {
     TreeNode* parent = nullptr;
 };
 
-template <class T>
-void PrintTree(const TreeNodePtr<T> root, std::ostream& out = std::cout) {
-    out << " ( "s;
-    out << root->value;
-    if (root->left || root->right) {
-        if (root->left) {
-            PrintTree(root->left, out);
-        } else {
-            out << "*"s;
-        }
-        if (root->right) {
-            PrintTree(root->right, out);
-        } else {
-            out << "*"s;
-        }
-    }
-    out << " ) "s;
-}
-
-template <class T>
-std::ostream& operator<<(std::ostream& out, const TreeNodePtr<T> node) {
-    PrintTree<T>(node, out);
-    return out;
-}
-
 template <typename T>
 bool CheckTreeProperty(const TreeNodePtr<T>& node, const T* min, const T* max) {
     if (node == nullptr) return true;
-    const T value = node.get()->value;
+    const T& value = node.get()->value;
     if ((min && value <= *min) || (max && value >= *max)) {
         return false;
     }
-    return CheckTreeProperty(node->left, min, &value) && CheckTreeProperty(node->right, &value, max);
+    return CheckTreeProperty(node.get()->left, min, &value) && CheckTreeProperty(node.get()->right, &value, max);
 }
 
 template <typename T>
-bool CheckTreeProperty(const TreeNodePtr<T> node) {
+bool CheckTreeProperty(const TreeNodePtr<T>& node) {
     return CheckTreeProperty<T>(node, nullptr, nullptr);
 }
 
 template <typename T>
 bool CheckTreeProperty(const T* node) {
-    return CheckTreeProperty<T>(std::make_unique<TreeNode<T>>(node));
+    return CheckTreeProperty<T>(std::make_unique<TreeNode<T>>(node, nullptr, nullptr));
 }
 
 TreeNodePtr<int> N(int val, TreeNodePtr<int>&& left = {}, TreeNodePtr<int>&& right = {}) {
-    return std::make_unique<TreeNode<int>>(std::move(val), std::move(left), std::move(right));
+    auto res = std::make_unique<TreeNode<int>>(val, std::move(left), std::move(right));
+    if (left) {
+        left->parent = res.get();
+    }
+    if (right) {
+        right->parent = res.get();
+    }
+
+    return res;
 }
 
 template <class T>
@@ -101,12 +84,14 @@ int main() {
     using namespace std;
     using T = TreeNode<int>;
     auto root1 = N(6, N(4, N(3), N(5)), N(7));
+    assert(CheckTreeProperty(root1));
     assert(CheckTreeProperty(root1.get()));
 
     auto root2 = N(6, 
         N(4, 
         N(3), N(5) ), 
             N(7, N(8)));
+    assert(!CheckTreeProperty(root2));
     assert(!CheckTreeProperty(root2.get()));
 
     // Функция DeleteTree не нужна. Узлы дерева будут рекурсивно удалены
